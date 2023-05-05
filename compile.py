c1a313d53f52d692249c50ecee44b4b82776c57c
whmsft <whmsft@outlook.com>
2023-05-05 19:53:18 +0530

    Add variable declarations to dictionary in compile.py

--- compile.py
+++ compile.py
@@ -6,11 +6,13 @@ class regex:
   requ = r'require "(.+)" @(.+)'
   func = r'\.(\w+)\((\w*)\) \{(.+?)\n\}'
   call = r'\$(\w+)\.(\w+)\s*\{([^}]+)\}'
+  vars = r'#\s*(\w+)\s*:\s*(\w+)\s*;?'
 
 # Use re.sub() to replace the matched pattern with the desired output format
 _code = re.sub(regex.requ, r'import \1 from \2',  code)
 _code = re.sub(regex.func, r'def \1(\2):\3', _code, flags=re.DOTALL)
 _code = re.sub(regex.call,  lambda m: f'{m.group(1)}.{m.group(2)}(' + ', '.join(f'"{k.strip()}"={v.strip()}' for k,v in re.findall(r'(\w+)\s*:\s*([^;]+);', m.group(3))) + ')', _code)
+_code = re.sub(regex.vars, lambda m: f'vars["{m.group(1)}"] = {m.group(2)}', _code)
 
 print(_code)
 
